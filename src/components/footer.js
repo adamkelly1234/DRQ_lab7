42135aa537675375ddc2ee830ce05109dbbfec20
Adam Kelly <G00367094@gmit.ie>
2020-11-12 18:52:32 +0000

    Lab Upload

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Footer extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>This is the Footer Component.</h1>
+            </div>
+        );
+    }
+}
